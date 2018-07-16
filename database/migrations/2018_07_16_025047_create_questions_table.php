25a01e3d9a7446aa88ead89139543252e32c66c9
Quan Nguyen <quannh18895@gmail.com>
2018-07-16 13:31:54 +0700

    add migration

---
+++ database/migrations/2018_07_16_025047_create_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idExam')->unsigned();
+            $table->foreign('idExam')->references('id')->on('exams')->onDelete('cascade');
+            $table->longText('description');
+            $table->smallInteger('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
